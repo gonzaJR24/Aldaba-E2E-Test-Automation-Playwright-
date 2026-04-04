272640e3f3746ce382ca5fafc35b072ea8b87924
junior <andresjunior240498hehe@gmail.com>
2026-04-03 20:36:45 -0400

    refactor: improvement in logic separation and scalability of tests

---
+++ data/language-data.ts
@@ -0,0 +1,38 @@
+ interface languageInterface {
+    name: string;
+    value: string;
+    title: string;
+  }
+
+export const languages: languageInterface[] = [
+    {
+      name: "english",
+      value: "index.php?lang=en",
+      title: "Employment and training portal",
+    },
+    {
+      name: "spanish",
+      value: "index.php?lang=es",
+      title: "Portal de empleo y formación",
+    },
+    {
+      name: "french",
+      value: "index.php?lang=fr",
+      title: "Portail de l’emploi et de la formation",
+    },
+    {
+      name: "italian",
+      value: "index.php?lang=it",
+      title: "Portale dell'occupazione e della formazione",
+    },
+    {
+      name: "portuguese",
+      value: "index.php?lang=pt",
+      title: "Portal do emprego e da formação",
+    },
+    {
+      name: "romanian",
+      value: "index.php?lang=ro",
+      title: "Ocuparea forţei de muncă şi de formare",
+    },
+  ];
--- tests/countries.spec.ts
+++ tests/countries.spec.ts
@@ -1,5 +1,6 @@
 import test, { expect } from "@playwright/test";
 import { Countries } from "../pages/countries";
+import {languages} from '../data/language-data'
 
 test.describe("countries test suite", () => {
   let countries: Countries;
@@ -8,54 +9,10 @@ test.describe("countries test suite", () => {
     await countries.goTo();
   });
 
-  test("check languages change", async ({ page }) => {
-    await test.step("validate english language", async () => {
-      await countries.expectLanguageChange(
-        "index.php?lang=en",
-        "Employment and training portal",
-      );
-    });
 
-    await test.step("validate spanish language", async () => {
-      await countries.expectLanguageChange(
-        "index.php?lang=es",
-        "Portal de empleo y formación",
-      );
+  for (let language of languages) {
+    test(`validate ${language.name} language`, async () => {
+      await countries.expectLanguageChange(language.value, language.title);
     });
-
-    await test.step("validate french language", async () => {
-      await countries.expectLanguageChange(
-        "index.php?lang=fr",
-        "Portail de l’emploi et de la formation",
-      );
-    });
-
-    await test.step("validate italian language", async () => {
-      await countries.expectLanguageChange(
-        "index.php?lang=it",
-        "Portale dell'occupazione e della formazione",
-      );
-    });
-
-    await test.step("validate haitian language", async () => {
-      await countries.expectLanguageChange(
-        "index.php?lang=ht",
-        "Travay ak pòtal fòmasyon",
-      );
-    });
-
-    await test.step("validate portuguese language", async () => {
-      await countries.expectLanguageChange(
-        "index.php?lang=pt",
-        "Portal do emprego e da formação",
-      );
-    });
-
-    await test.step("validate romanian language", async () => {
-      await countries.expectLanguageChange(
-        "index.php?lang=ro",
-        "Ocuparea forţei de muncă şi de formare",
-      );
-    });
-  });
+  }
 });
