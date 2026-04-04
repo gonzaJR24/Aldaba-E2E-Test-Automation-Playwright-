277d966f30b280846e218eebf2276a60d5da2e6e
junior <andresjunior240498hehe@gmail.com>
2026-04-04 10:42:46 -0400

    test: add tests for multiple country selections

--- tests/countries.spec.ts
+++ tests/countries.spec.ts
@@ -1,6 +1,7 @@
 import test, { expect } from "@playwright/test";
 import { Countries } from "../pages/countries";
 import {languages} from '../data/language-data'
+import { CountryData } from "../data/countries-data";
 
 test.describe("countries test suite", () => {
   let countries: Countries;
@@ -15,4 +16,13 @@ test.describe("countries test suite", () => {
       await countries.expectLanguageChange(language.value, language.title);
     });
   }
+
+  for(let country of CountryData){
+    test(`verify country: ${country}`, async({page})=>{
+      await page.locator('select[name="geo"]').selectOption(`index.php?newg=${country}`)
+      await expect(page.locator('a', {hasText:country})).toBeVisible()
+
+    })
+  }
 });
+
